e241c51280d0fcb919677d033657ee0dc6de1113
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-15 12:21:15 -0800

    Adds server code and text thoughts

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+})
+
+var thoughts = [
+  {text: "First thought", time: "First time"},
+  {text: "Second thought", time: "Second time"}
+];
+
+app.use(express.static(__dirname + "/public"));
+
+// JSON.stringify turns a Javascript object into JSON text and stores that JSON text in a string.
+app.get("/thoughts", function(request, response){
+  response.type("json");
+  response.end(JSON.stringify({thoughts:thoughts}));
+});
